390f21a0a762b8fed14b2e1eb0b4df5c422e0c4d
Nathan Mossaad <git@nathanmossaad.com>
2024-08-27 21:27:37 +0200

    Intial Commit

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import psycopg
+
+conn = psycopg.connect(host="db", user="postgres", password="postgres")
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/db_test")
+def read_db_test():
+    # Missing 
+    with conn.cursor() as cur:
+        # Pass data to fill a query placeholders and let Psycopg perform
+        # the correct conversion (no SQL injections!)
+        cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
+
+        # Query the database and obtain data as Python objects.
+        cur.execute("SELECT * FROM test")
+
+        # Make the changes to the database persistent
+        conn.commit()
+
+        return cur.fetchall()
